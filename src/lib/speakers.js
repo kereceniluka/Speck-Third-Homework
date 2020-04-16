e82f7d94608ea47410e4af399dcc8719e9197ffb
Luka Kereceni <luka.kereceni@gmail.com>
2020-04-16 19:17:10 +0200

    Data Fix

--- src/lib/speakers.js
+++ src/lib/speakers.js
@@ -1,31 +1,31 @@
 const speakers = [
     {
-        title: 'Johan Back',
+        title: 'Johan Bach',
         about: 'Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one.',
         link: 'Prati sudionika',
     },
     {
-        title: 'Johan Cobra',
+        title: 'Sebastian Back',
         about: 'Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one.',
         link: 'Prati sudionika',
     },
     {
-        title: 'Johan Back',
+        title: 'Johan Franck',
         about: 'Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one.',
         link: 'Prati sudionika',
     },
     {
-        title: 'Johan Back',
+        title: 'Johan Speck',
         about: 'Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one.',
         link: 'Prati sudionika',
     },
     {
-        title: 'Johan Back',
+        title: 'Joe Boe',
         about: 'Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one.',
         link: 'Prati sudionika',
     },
     {
-        title: 'Johan Back',
+        title: 'El Bow',
         about: 'Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one.',
         link: 'Prati sudionika',
     }
