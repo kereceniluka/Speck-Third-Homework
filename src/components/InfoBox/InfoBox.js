5eb4264decb0703a8aae9b243ad2d13e4bf0304f
Luka Kereceni <luka.kereceni@gmail.com>
2020-04-26 23:37:31 +0200

    Little fix on InfoBox__Btn-Join text

--- src/components/InfoBox/InfoBox.js
+++ src/components/InfoBox/InfoBox.js
@@ -32,7 +32,7 @@ const InfoBox = props => {
             {props.isTypeEvent ? displayInformationSection(props) : null}
             <p className="InfoBox__About">{props.about}</p>
             <div className="InfoBox__Footer">
-                <Link className="InfoBox__Btn-Join" to={props.link}>{props.link}</Link>
+                <Link className="InfoBox__Btn-Join" to={props.link}>{props.isTypeEvent ? <span>Prijavi se na predavanje</span> : <span>Prati sudionika</span>}</Link>
             </div>
         </div>
     );
